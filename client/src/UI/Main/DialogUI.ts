7fc79a9927b4e097678cf28a999c0e5b4ce18047
azoth <zhou.vip@icloud.com>
2018-04-29 00:29:30 +0800

    pay

--- client/src/UI/Main/DialogUI.ts
+++ client/src/UI/Main/DialogUI.ts
@@ -239,7 +239,7 @@ class ShopContentIRUI extends eui.ItemRenderer {
     private buyToolfunc(){
         var request = new egret.HttpRequest();
         request.responseType = egret.HttpResponseType.TEXT;
-        request.open(encodeURI(`http://101.37.151.85:8008/socket/wx/preOrder`),egret.HttpMethod.GET);
+        request.open(encodeURI(`http://101.37.151.85:8008/socket/wx/preOrder?openid=${GameMode.wechatId}&buyNum=0.01`),egret.HttpMethod.GET);
         request.send();
         request.addEventListener(egret.Event.COMPLETE,(evt)=>{
             var response = <egret.HttpRequest>evt.currentTarget;
@@ -247,8 +247,8 @@ class ShopContentIRUI extends eui.ItemRenderer {
             wx.chooseWXPay({  
                 timestamp: res.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符  
                 nonceStr: res.nonce_str, // 支付签名随机串，不长于 32 位  
-                package: res.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）  
-                signType: 'SHA1', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'  
+                package: `prepay_id=${res.prepay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）  
+                signType: 'HMAC-SHA256', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'  
                 paySign: res.sign, // 支付签名  
                 success: function (res) {
                     // 支付成功后的回调函数  
